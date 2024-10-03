3ffab28e08ba3d2065c07f1c396a9d33f2d0cea5
vijay <Vijaysitamsetty@gmail.com>
2024-10-03 17:44:20 +0530

    first

---
+++ sign-inbox.js
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+document.addEventListener("DOMContentLoaded", function () {
+const signinBox=document.getElementById("signin-box");
+signinBox.addEventListener("click", function () {
+window.location.href = "cloneimages/sign-in.html";
+    });
+
+signinBox.style.cursor = "pointer";
+});
+=======
+document.addEventListener("DOMContentLoaded", function () {
+const signinBox=document.getElementById("signin-box");
+signinBox.addEventListener("click", function () {
+window.location.href = "cloneimages/sign-in.html";
+    });
+
+signinBox.style.cursor = "pointer";
+});
+>>>>>>> 3522e5e572a4a2223c2490efd7e44419925e53c3
